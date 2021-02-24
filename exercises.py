b3d3e88ca6cff3c6e42faa38d40a7e930583c6ab
angella <angella@laboremus.no>
2021-02-24 23:29:17 +0300

    More exercises

--- exercises.py
+++ exercises.py
@@ -1,6 +1,16 @@
 import requests
 from bs4 import BeautifulSoup as bs
 # load web page
-request = requests.get("https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbngwamdicExVWEZicFRPdHM5b1BwUmJpRjlJUXxBQ3Jtc0tra1JEdGhpV2d4azV4NDM5QmNVVXp4TGNSRUNMVF9VWVhWc3AwX2RMME5KRURieFdtZzUzY1ZxU3kxcWVvR1M3bFZXckwxVUxuc3kxSXZhU3hsYmFvb3I2MkN2ak1pQTVaNmZnNlh4Q05FcEVUR2dmdw&q=https%3A%2F%2Fkeithgalli.github.io%2Fweb-scraping%2Fexample.html")
-soup = bs(r.content, "html.parser")
-soup
+request = requests.get("https://keithgalli.github.io/web-scraping/webpage.html")
+soup = bs(request.content, "html.parser")
+# print(soup.prettify)
+
+# Grab all of the social links from the web page. Do it in three different ways
+links = soup.select("link")
+# print(links)
+links_2 = soup.find_all("link")
+# print(links_2)
+links_3 = soup.select("ul li a", attrs={"href": "https://www.instagram.com/keithgalli/"})
+actual_links = [link["href"] for link in links_3]
+print(links_3)
+# 
